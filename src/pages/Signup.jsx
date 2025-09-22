e573cad08f91a551e338253a68781e1f1810c976
chipjs360 <josephchipate@gmail.com>
2025-09-22 16:00:17 +0200

    sigup updates

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,14 +1,21 @@
 import '../assets/css/Signup.css'
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import API from "../api";
 import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
   const [formData, setFormData] = useState({
+    fullname: "",
     username: "",
     email: "",
     password: "",
+    role: "",
+    department: "",
+    job_title: "",
+    employee_id: "",
   });
+  const [departments, setDepartments] = useState([]);
+  const [roles, setRoles] = useState([]);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -26,6 +33,20 @@ export default function Signup() {
       alert("Signup failed");
     }
   };
+  useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const rolesRes = await API.get("/api/auth/roles/");
+          setRoles(rolesRes.data);
+
+          const deptRes = await API.get("api/auth/departments/");
+          setDepartments(deptRes.data);
+        } catch (err) {
+          console.error("Error loading roles/departments", err);
+        }
+      };
+      fetchData();
+    }, []);
   return (
     <div className="signup-page"> {/* scoped wrapper */}
       <div className="auth-card text-white mx-3">
@@ -63,6 +84,57 @@ export default function Signup() {
             placeholder="Password" 
             onChange={handleChange}
             required />
+          </div>
+          <div className="mb-3">
+            <select
+              name="role"
+              className="form-control"
+              onChange={handleChange}
+              required
+            >
+              <option value="">Select Role</option>
+              {roles.map((role) => (
+                <option key={role.value} value={role.value}>
+                  {role.label}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="mb-3">
+            <select
+              name="department"
+              className="form-control"
+              onChange={handleChange}
+              required
+            >
+              <option value="">Select Department</option>
+              {departments.map((dept) => (
+                <option key={dept.id} value={dept.id}>
+                  {dept.name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="mb-3">
+            <input
+              type="text"
+              name="job_title"
+              className="form-control"
+              placeholder="Job Title"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3">
+            <input
+              type="text"
+              name="employee_id"
+              className="form-control"
+              placeholder="Employee ID"
+              onChange={handleChange}
+            />
           </div>
           <button type="submit" className="btn btn-custom w-100">Sign Up</button>
         </form>
