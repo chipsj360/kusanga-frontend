5de9f3d98e7c0e58e4ff09d7e179e1d90b3607ba
chipjs360 <josephchipate@gmail.com>
2026-04-09 13:44:00 +0200

    auth edits

--- src/pages/EditUser.jsx
+++ src/pages/EditUser.jsx
@@ -42,7 +42,7 @@ const EditUser = ({ user, onClose, onSuccess }) => {
                 <div className="col-md-6 mb-3">
                   <label>Role</label>
                   <select name="role" value={form.role} className="form-select" onChange={handleChange}>
-                    <option value="employee">Employee</option>
+                    <option value="student">Student</option>
                     <option value="trainer">Trainer</option>
                     <option value="admin">Admin</option>
                   </select>
